7a1b92df9d9d08f5bd0c6a2f77fddb13a5dd6996
Vasiliy Sizov <pinky-77@yandex.ru>
2023-11-17 11:44:06 +0300

    Added parameterization in BunTest.java IngredientTest.java

--- src/test/java/praktikum/BunTest.java
+++ src/test/java/praktikum/BunTest.java
@@ -1,15 +1,48 @@
 package praktikum;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 
+import static praktikum.constants.Constants.*;
+
+@RunWith(Parameterized.class)
 public class BunTest {
+    private final int bunNumber;
+    private final String expBunName;
+    private final float expBunPrice;
+    public BunTest(int bunNumber, String expBunName, float expBunPrice) {
+        this.bunNumber = bunNumber;
+        this.expBunName = expBunName;
+        this.expBunPrice = expBunPrice;
+    }
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+    @Spy
+    Database database = new Database();
+    Bun bun;
 
-    Bun bun = new Bun("atomic bun", 100.55F);
-
+    @Parameterized.Parameters(name = "Тест булки. Тестовые данные: номер в базе-{0} название-{1} цена-{2}")
+    public static Object[][] params() {
+        return new Object[][] {
+                { 0, BUN_0_NAME, BUN_0_PRISE},
+                { 1, BUN_1_NAME, BUN_1_PRISE},
+                { 2, BUN_2_NAME, BUN_2_PRISE},
+        };
+    }
     @Test
     public void getNameTest() {
-        String expected = "atomic bun";
+        bun = database.availableBuns().get(bunNumber);
+        Mockito.verify(database, Mockito.times(1)).availableBuns();
+        Mockito.verifyNoMoreInteractions(database);
+        String expected = expBunName;
         String actual = bun.getName();
 
         Assert.assertEquals(expected, actual);
@@ -17,7 +50,10 @@ public class BunTest {
 
     @Test
     public void getPriceTest() {
-        Float expected = 100.55F;
+        bun = database.availableBuns().get(bunNumber);
+        Mockito.verify(database, Mockito.times(1)).availableBuns();
+        Mockito.verifyNoMoreInteractions(database);
+        Float expected = expBunPrice;
         Float actual = bun.getPrice();
 
         Assert.assertEquals(expected, actual);
--- src/test/java/praktikum/IngredientTest.java
+++ src/test/java/praktikum/IngredientTest.java
@@ -1,13 +1,52 @@
 package praktikum;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 
+import static praktikum.constants.Constants.*;
+
+@RunWith(Parameterized.class)
 public class IngredientTest {
-Ingredient ingredient = new Ingredient(IngredientType.SAUCE,"brown goo", 57.99F);
+    private final int ingredientNumber;
+    private final IngredientType expIngredientType;
+    private final String expIngredientName;
+    private final float expIngredientPrice;
+    public IngredientTest(int ingredientNumber, IngredientType expIngredientType, String expIngredientName, float expIngredientPrice) {
+        this.ingredientNumber = ingredientNumber;
+        this.expIngredientType = expIngredientType;
+        this.expIngredientName = expIngredientName;
+        this.expIngredientPrice = expIngredientPrice;
+    }
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+    @Spy
+    Database database = new Database();
+    Ingredient ingredient;
+
+    @Parameterized.Parameters(name = "Тест ингредиента. Тестовые данные: номер в базе-{0} тип-{1} название-{2} цена-{3}")
+    public static Object[][] params() {
+        return new Object[][] {
+                { 0, SAUCE_TYPE, SAUCE_0_NAME, SAUCE_0_PRISE},
+                { 1, SAUCE_TYPE, SAUCE_1_NAME, SAUCE_1_PRISE},
+                { 2, SAUCE_TYPE, SAUCE_2_NAME, SAUCE_2_PRISE},
+                { 3, FILLING_TYPE, FILLING_0_NAME, FILLING_0_PRISE},
+                { 4, FILLING_TYPE, FILLING_1_NAME, FILLING_1_PRISE},
+                { 5, FILLING_TYPE, FILLING_2_NAME, FILLING_2_PRISE},
+        };
+    }
     @Test
     public void getPriceTest() {
-        Float expected = 57.99F;
+        ingredient = database.availableIngredients().get(ingredientNumber);
+
+        Float expected = expIngredientPrice;
         Float actual = ingredient.getPrice();
 
         Assert.assertEquals(expected, actual);
@@ -15,7 +54,10 @@ Ingredient ingredient = new Ingredient(IngredientType.SAUCE,"brown goo", 57.99F)
 
     @Test
     public void getNameTest() {
-        String expected = "brown goo";
+        ingredient = database.availableIngredients().get(ingredientNumber);
+        Mockito.verify(database, Mockito.times(1)).availableIngredients();
+        Mockito.verifyNoMoreInteractions(database);
+        String expected = expIngredientName;
         String actual = ingredient.getName();
 
         Assert.assertEquals(expected, actual);
@@ -23,7 +65,10 @@ Ingredient ingredient = new Ingredient(IngredientType.SAUCE,"brown goo", 57.99F)
 
     @Test
     public void getTypeTest() {
-        IngredientType expected = IngredientType.SAUCE;
+        ingredient = database.availableIngredients().get(ingredientNumber);
+        Mockito.verify(database, Mockito.times(1)).availableIngredients();
+        Mockito.verifyNoMoreInteractions(database);
+        IngredientType expected = expIngredientType;
         IngredientType actual = ingredient.getType();
 
         Assert.assertEquals(expected, actual);
